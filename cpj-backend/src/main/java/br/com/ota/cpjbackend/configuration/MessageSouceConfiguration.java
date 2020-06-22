1f25157844870c8f83d4b4248e01ff4adc558ef0
Olivan Aires <olivanaires@gmail.com>
2020-06-22 12:42:37 -0300

    configuração de internacionalização

---
+++ cpj-backend/src/main/java/br/com/ota/cpjbackend/configuration/MessageSouceConfiguration.java
@@ -0,0 +1,18 @@
+package br.com.ota.cpjbackend.configuration;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+@Configuration
+public class MessageSouceConfiguration {
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.addBasenames("classpath:messages");
+        return messageSource;
+    }
+
+}
--- cpj-backend/src/main/java/br/com/ota/cpjbackend/controller/AuthController.java
+++ cpj-backend/src/main/java/br/com/ota/cpjbackend/controller/AuthController.java
@@ -1,17 +1,22 @@
 package br.com.ota.cpjbackend.controller;
 
 import br.com.ota.cpjbackend.configuration.JwtTokenProvider;
+import br.com.ota.cpjbackend.exception.AppException;
 import br.com.ota.cpjbackend.model.User;
 import br.com.ota.cpjbackend.model.vo.LoginRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Locale;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -21,18 +26,24 @@ public class AuthController {
 
     private final AuthenticationManager authenticationManager;
     private final JwtTokenProvider tokenProvider;
+    private final MessageSource messageSource;
 
     @PostMapping("/signin")
     public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
-        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+        try{
+            Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
 
-        SecurityContextHolder.getContext().setAuthentication(authentication);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
 
-        User user = (User) authentication.getPrincipal();
-        String token = tokenProvider.generateToken(user);
-        user.setToken(token);
+            User user = (User) authentication.getPrincipal();
+            String token = tokenProvider.generateToken(user);
+            user.setToken(token);
 
-        return ResponseEntity.ok(user);
+            return ResponseEntity.ok(user);
+
+        } catch (BadCredentialsException ex) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new AppException(messageSource.getMessage("invalid.user", null, new Locale("pt", "BR"))));
+        }
     }
 
 }
