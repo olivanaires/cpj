5090161da5cb32c874bd9b1034db0146ee62545f
Olivan Aires <olivanaires@gmail.com>
2020-08-17 09:17:02 -0300

    correção consulta pagamentos para receber

--- cpj-backend/src/main/java/br/com/ota/cpjbackend/repository/ContractRepository.java
+++ cpj-backend/src/main/java/br/com/ota/cpjbackend/repository/ContractRepository.java
@@ -6,7 +6,6 @@ import br.com.ota.cpjbackend.model.Lawyer;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.Date;
 import java.util.List;
 
 @Repository
@@ -16,10 +15,7 @@ public interface ContractRepository extends BaseRepository<Contract, Long> {
 
     List<Contract> findAllByHiredContains( Lawyer lawyer );
 
-    @Query(value = "select cont from Contract cont where cont.endDate > now() and (cont.paymentType = 'MONTHLY' or (cont.paymentType = 'YEARLY' and extract( 'month' from cont.paymentDate) = extract( 'month' from now())))")
+    @Query(value = "select cont from Contract cont where cont.endDate > now() and (cont.paymentType in ('MONTHLY', 'MONTHLY_HONORARY') or (cont.paymentType in ('YEARLY', 'YEARLY_HONORARY') and extract( 'month' from cont.paymentDate) = extract( 'month' from now())))")
     List<Contract> findAllWithPaymentForCurrentlyMonth();
-
-    @Query(nativeQuery = true, value = "select cont from contracts cont where end_date > now() and (payment_type = 'MONTHLY' or (payment_type = 'YEARLY' and extract('month' from payment_date) = ?1))")
-    List<Contract> findAllWithPaymentByMonth(int month);
 
 }
