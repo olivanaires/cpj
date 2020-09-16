8f5120b68c3defe0088dfd97aa6fa9697df3f0b3
Olivan Aires <olivanaires@gmail.com>
2020-09-16 13:20:55 -0300

    atualização de informações no banco

--- cpj-backend/src/main/resources/data.sql
+++ cpj-backend/src/main/resources/data.sql
@@ -1,6 +1,9 @@
+-- Para não dar erro ao executar
 select now();
--- update payments set payment_type = 'MONTHLY';
--- alter table contracts alter column duration_type type varchar(10);
--- update contracts set duration_type='UNIQUE' where duration_type='0';
--- update contracts set duration_type='MONTH' where duration_type='1';
--- update contracts set duration_type='YEAR' where duration_type='2';
+
+-- Atualizações a serem feitas
+update payments set payment_type = 'MONTHLY';
+alter table contracts alter column duration_type type varchar(10);
+update contracts set duration_type='UNIQUE' where duration_type='0';
+update contracts set duration_type='MONTH' where duration_type='1';
+update contracts set duration_type='YEAR' where duration_type='2';
